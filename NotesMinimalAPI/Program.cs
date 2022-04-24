d8cabf411c35a5e71e58d848030a1fc1540b0bbc
Hércules S. S. José <hercules_hssj@outlook.com>
2022-04-24 12:35:37 -0300

    Implementado a API  REST

--- NotesMinimalAPI/Program.cs
+++ NotesMinimalAPI/Program.cs
@@ -6,6 +6,18 @@ References:
 - https://www.tutlinks.com/minimal-web-api-with-crud-on-postgresql-dotnet-6/
 */
 
+/* API implementation
+
+HTTP VERB   ENDPOINT        STATUS CODE(S)      	DESCRIPTION
+POST	    /notes	        201 Created 	        Creates a Note
+GET	        /notes/{id:int} 200 OK, 404 Not Found   Read a Note given its id
+GET	        /notes          200 OK                  Retrieves all Notes
+PUT	        /notes/{id:int} 200 OK, 404 Not Found,
+                            400 Bad Request	        Updates a Note given its id
+DELETE	    /notes/{id:int} 204 No Content          Deletes a Note given its id
+
+*/
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Configure JSON logging to the console.
@@ -38,6 +50,52 @@ if (app.Environment.IsDevelopment())
 }
 
 app.MapGet("/", () => "Welcome to Notes API!");
+
+app.MapPost("/notes/", async(Note n, NoteDb db)=> {
+    db.Notes.Add(n);
+    await db.SaveChangesAsync();
+
+    return Results.Created($"/notes/{n.id}", n);
+});
+
+app.MapGet("/notes/{id:int}", async(int id, NoteDb db)=> {
+    return await db.Notes.FindAsync(id)
+            is Note n
+                ? Results.Ok(n)
+                : Results.NotFound();
+});
+
+app.MapGet("/notes", async (NoteDb db) =>  {
+    return await db.Notes.ToListAsync();
+});
+
+app.MapPut("/notes/{id:int}", async(int id, Note n, NoteDb db)=>
+{
+    if (n.id != id)
+    {
+        return Results.BadRequest();
+    }
+
+    var note = await db.Notes.FindAsync(id);
+    
+    if (note is null) return Results.NotFound();
+
+    //found, so update with incoming note n.
+    note.text = n.text;
+    note.done = n.done;
+    await db.SaveChangesAsync();
+    return Results.Ok(note);
+});
+
+app.MapDelete("/notes/{id:int}", async(int id, NoteDb db)=>{
+
+    var note = await db.Notes.FindAsync(id);
+    if (note is not null){
+        db.Notes.Remove(note);
+        await db.SaveChangesAsync();
+    }
+    return Results.NoContent();
+});
 
 await app.RunAsync();
 
