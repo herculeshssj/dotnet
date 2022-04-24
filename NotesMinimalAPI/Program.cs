7b6af362306804b699f1e9843ede85083dbdf6c3
Hércules S. S. José <hercules_hssj@outlook.com>
2022-04-24 11:16:12 -0300

    Configuração da base de dados.

--- NotesMinimalAPI/Program.cs
+++ NotesMinimalAPI/Program.cs
@@ -1,5 +1,9 @@
+using Microsoft.EntityFrameworkCore; // place this line at the beginning of file.
+
 /*
-Reference: https://www.tutlinks.com/getting-started-with-minimal-web-api-asp-net-core/
+References: 
+- https://www.tutlinks.com/getting-started-with-minimal-web-api-asp-net-core/
+- https://www.tutlinks.com/minimal-web-api-with-crud-on-postgresql-dotnet-6/
 */
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,6 +15,13 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Connect to PostgreSQL Database
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<NoteDb>(options =>
+    options.UseNpgsql(connectionString));
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 var app = builder.Build();
 
@@ -29,3 +40,16 @@ if (app.Environment.IsDevelopment())
 app.MapGet("/", () => "Welcome to Notes API!");
 
 await app.RunAsync();
+
+
+record Note(int id){
+    public string text {get;set;} = default!;
+    public bool done {get;set;} = default!;
+}
+
+class NoteDb: DbContext {
+    public NoteDb(DbContextOptions<NoteDb> options): base(options) {
+
+    }
+    public DbSet<Note> Notes => Set<Note>();
+}
