2dd3921ed97f6d6b5596b45711f335d2d1459f55
Hércules S. S. José <hercules_hssj@outlook.com>
2022-04-24 10:52:55 -0300

    Importado os arquivos do projeto.

---
+++ NotesMinimalAPI/Program.cs
@@ -0,0 +1,77 @@
+/*
+Reference: https://www.tutlinks.com/getting-started-with-minimal-web-api-asp-net-core/
+*/
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Configure JSON logging to the console.
+// builder.Logging.AddJsonConsole();
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Setting the application ports
+// Application ports are set on appsettings.json
+// app.Urls.Add("https://0.0.0.0:7243");
+// app.Urls.Add("http://0.0.0.0:7242");
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapGet("/", () => "Welcome to Notes API!");
+
+await app.RunAsync();
+
+/*
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast =  Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateTime.Now.AddDays(index),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+app.Run();
+
+record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+*/
---
+++ NotesMinimalAPI/README.md
@@ -0,0 +1,20 @@
+NotesMinimalAPI
+===============
+
+Port configuration from the command line argument --urls
+
+```
+dotnet run --urls="https://localhost:5001;http://localhost:5000"
+```
+
+Run project :
+
+```
+dotnet run
+```
+
+Enable hot reload
+
+```
+dotnet watch run
+```
