4e9c8dce6c1c2a02145e02a866c5dc265d706f8e
Hércules S. S. José <hercules_hssj@outlook.com>
2022-04-24 22:30:39 -0300

    Incluído a porta https.

--- OportunidadeAPI/Program.cs
+++ OportunidadeAPI/Program.cs
@@ -22,6 +22,7 @@ app.UseSwaggerUI();
 app.UseHttpsRedirection();
 
 app.Urls.Add("http://0.0.0.0:7242");
+app.Urls.Add("https://0.0.0.0:7243");
 
 /* GET - /concursos - Todos os concursos */
 app.MapGet("/concursos", async (OportunidadeDb db) =>  {
