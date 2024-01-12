db956d1fa0cf4929f40714b7788f605787745837
Hércules S. S. José <hercules_hssj@outlook.com>
2024-01-12 19:17:42 -0300

    Realização de exercício

--- csharp-course/SaidaDados/SaidaDados/Program.cs
+++ csharp-course/SaidaDados/SaidaDados/Program.cs
@@ -10,14 +10,14 @@ Console.WriteLine("Boa noite!");
 
 // Impressão de variáveis no console
 Console.WriteLine("*** Impressão de variáveis no console ***");
-char genero = 'F';
-int idade = 32;
+char genero1 = 'F';
+int idade1 = 32;
 double saldo = 10.35784;
 string nome = "Maria";
 
-Console.WriteLine(genero);
+Console.WriteLine(genero1);
 Console.WriteLine(nome);
-Console.WriteLine(idade);
+Console.WriteLine(idade1);
 Console.WriteLine(saldo);
 
 // Imprimindo valores double controlando o número de casas decimais
@@ -31,15 +31,44 @@ Console.WriteLine(saldo.ToString("F3", CultureInfo.InvariantCulture));
 
 // Usando placeholders
 Console.WriteLine("{0} tem {1} anos, é do gênero {2} e tem {3} reais de saldo no banco.",
-    nome, idade, genero, saldo);
+    nome, idade1, genero1, saldo);
 
 // Usando placeholders e arredondando números double
 Console.WriteLine("{0} tem {1} anos, é do gênero {2} e tem {3:F2} reais de saldo no banco.",
-    nome, idade, genero, saldo);
+    nome, idade1, genero1, saldo);
 
 // Usando interpolação - disponível do C# 6.0 em diante
-Console.WriteLine($"{nome} tem {idade} anos, é do gênero {genero} e tem {saldo:F2} reais de saldo no banco.");
+Console.WriteLine($"{nome} tem {idade1} anos, é do gênero {genero1} e tem {saldo:F2} reais de saldo no banco.");
 
 // Usando concatenação (igual em qualquer outra linguagem)
-Console.WriteLine(nome + " tem " + idade + " anos, é do gênero " + genero + " e tem " + saldo.ToString("F2") + " reais de saldo no banco.");
+Console.WriteLine(nome + " tem " + idade1 + " anos, é do gênero " + genero1 + " e tem " + saldo.ToString("F2") + " reais de saldo no banco.");
 
+Console.WriteLine();
+Console.WriteLine("**********************");
+Console.WriteLine("***** EXERCÍCIOS *****");
+Console.WriteLine("**********************");
+Console.WriteLine();
+
+string produto1 = "Computador";
+string produto2 = "Mesa de escritório";
+
+byte idade = 30;
+int codigo = 5290;
+char genero = 'M';
+
+double preco1 = 2100;
+double preco2 = 650.50;
+double medida = 53.234567;
+
+Console.WriteLine("Produtos:");
+Console.WriteLine($"{produto1}, cujo preço é $ {preco1}");
+Console.WriteLine($"{produto2}, cujo preço é $ {preco2}");
+Console.WriteLine();
+Console.WriteLine($"Registro: {idade} anos de idade, código {codigo} e gênero: {genero}");
+Console.WriteLine();
+Console.WriteLine($"Medida com oito casas decimais: {medida:F8}");
+Console.WriteLine($"Arredondado (três casas decimais): {medida:F3}");
+Console.WriteLine($"Separador decimal invariant culture: {medida.ToString("F3", CultureInfo.InvariantCulture)}");
+
+Console.WriteLine();
+Console.WriteLine("***** FIM DOS EXERCÍCIOS *****");
