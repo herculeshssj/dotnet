a1b7cea2b042a3a9846378880f09eeea4915c5c2
Hércules S. S. José <hercules_hssj@outlook.com>
2024-03-03 11:40:09 -0300

    Estruturas condicionais

---
+++ csharp-course/EstruturaCondicional/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace EstruturaCondicional;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Estrutura condicional (if-else)");
+        Console.WriteLine("-------------------------------");
+    
+        int a = 10;
+
+        Console.WriteLine("Bom dia");
+
+        if (a > 5) {
+            Console.WriteLine("Boa tarde");
+        }
+        
+        Console.WriteLine("Boa noite");
+
+        Console.WriteLine("-------------------------------");
+        Console.Write("Entre com um número inteiro: ");
+        int x = int.Parse(Console.ReadLine());
+
+        if (x % 2 == 0) {
+            Console.WriteLine("Par");
+        } else {
+            Console.WriteLine("Ímpar");
+        }
+
+        Console.WriteLine("-------------------------------");
+        Console.Write("Qual a hora atual? ");
+        int hora = int.Parse(Console.ReadLine());
+
+        if (hora < 12) {
+            Console.WriteLine("Bom dia!");
+        } else if (hora > 12 && hora < 18) {
+            Console.WriteLine("Boa tarde!");
+        } else if (hora >= 18) {
+            Console.WriteLine("Boa noite!");
+        } else {
+            Console.WriteLine("Hora inválida!");
+        }
+
+    }
+}
